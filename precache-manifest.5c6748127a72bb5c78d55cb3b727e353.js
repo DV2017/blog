40f2d4854292004ebc29f860c9e393d11dd577a1
DV <deepavarma.in@gmail.com>
2019-07-26 16:30:08 +0200

    Updates

---
+++ precache-manifest.5c6748127a72bb5c78d55cb3b727e353.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e56b45b4eb54b26819b02a4762d93514",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "fd04f4b949bce88bc680",
+    "url": "/blog/static/js/2.a85ab434.chunk.js"
+  },
+  {
+    "revision": "99018ac7ffc10ba47615",
+    "url": "/blog/static/js/main.8be8bcf2.chunk.js"
+  },
+  {
+    "revision": "eeecc51c3b4e3b0a67a2",
+    "url": "/blog/static/js/runtime~main.516a48d0.js"
+  }
+]);
